12403a0589da40288f2e907365b6515b022f1282
Davrun141 <33719036+Davrun141@users.noreply.github.com>
2018-05-01 23:38:34 +0100

    Bullets and Zombies Update
    Scripts and prefabs for bullets and zombies have been implemented

---
+++ UNIversiTY project/Assets/Scripts/Controls/ShootBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnRight;
+    public Transform bulletSpawnLeft;
+    public float fireTime = 0.5f;
+    private bool isFiring = false;
+    private string lastFireFrom = "Left";
+
+    void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    void Fire()
+    {
+        isFiring = true;
+        if (lastFireFrom == "Left")
+        {
+            lastFireFrom = "Right";
+            Instantiate(bulletPrefab, bulletSpawnRight.position, bulletSpawnRight.rotation);
+        } else
+        {
+            lastFireFrom = "Left";
+            Instantiate(bulletPrefab, bulletSpawnLeft.position, bulletSpawnLeft.rotation);
+        }
+
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+}
